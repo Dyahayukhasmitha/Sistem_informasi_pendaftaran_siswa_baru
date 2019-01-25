b76ce71ba7ed2ddf607c69a02b1545ca3c032e25
Dyahayukhasmitha <46661613+Dyahayukhasmitha@users.noreply.github.com>
2019-01-25 20:06:55 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,25 @@
+<style>
+.modal-header {
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+  background: linear-gradient(to bottom, #003366 0%, #336699 100%);
+
+}
+</style>
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat Datang di SI <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+              
+          </div>
+          </div>
+          </div>
+      
+    </div>
+    </div>
